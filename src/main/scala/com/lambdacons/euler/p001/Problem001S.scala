ce23cc2d20edd0bec5eb5f9d0700fc350c7831fb
Eric Somdahl <ericsomdahl@gmail.com>
2016-03-31 21:49:39 -0400

    scala problem 1

--- src/main/scala/com/lambdacons/euler/p001/Problem001S.scala
+++ src/main/scala/com/lambdacons/euler/p001/Problem001S.scala
@@ -5,7 +5,16 @@ package p001
   * Created by eric on 3/30/16.
   */
 object Problem001S {
+
   def sumOfMultiplesBelow(below : Int, multiples : Int*): Int = {
-    23
+    Stream.from(1, 1)
+      .takeWhile(_ < below)
+      .filter(value => {
+        multiples.exists( multiple => {
+          if (value % multiple == 0 ) true
+          else false
+        })
+      })
+      .sum
   }
 }
